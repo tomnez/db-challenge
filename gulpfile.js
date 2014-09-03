bc0ad84d05ca0e9f5c94f8cc3a0412fc11a872d3
Tom Netzband <tomnez@gmail.com>
2014-09-03 19:32:48 -0400

    spriteSheet: new sprite method

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,45 @@
-var gulp   = require('gulp')
-  , concat = require('gulp-concat')
-  , stylus = require('gulp-stylus')
-  , watch  = require('gulp-watch')
-  , nib    = require('nib');
+var gulp        = require('gulp')
+  , concat      = require('gulp-concat')
+  , stylus      = require('gulp-stylus')
+  , watch       = require('gulp-watch')
+  , spritesmith = require('gulp.spritesmith')
+  , es          = require('event-stream')
+  , temp        = require('temp').track()
+  , utils       = require('./lib/utils')()
+  , path        = require('path')
+  , nib         = require('nib');
 
-// STYLUS + NIB ---------------------------------------------------------- //
-gulp.task('nib', function () {
+// STYLUS, SPRITES, NIB -------------------------------------------------- //
+gulp.task('sprites', function() {
+  utils.createImageArray('./assets/css/spritesrc/', function(err, imageArray) {
+
+    if(err) { return console.error(err); }
+
+    var imgDestPath  = './public/css'
+      , stylDestPath = './assets/css';
+
+    var spriteOptions = {
+      'engine'      : 'gm', // use graphicsmagick
+      'cssName'     : 'sprites.styl',
+      'imgName'     : 'sprites.png',
+      'algorithm'   : 'binary-tree',
+      'cssTemplate' : path.join(__dirname, './templates/sprites.styl.mustache')
+    };
+
+    var spriteData = gulp.src(imageArray).pipe(spritesmith(spriteOptions))
+      , images = spriteData.img.pipe(gulp.dest(imgDestPath))
+      , stylus = spriteData.css.pipe(gulp.dest(stylDestPath));
+
+    return es.merge(images, stylus);
+  });
+});
+
+gulp.task('nib', ['sprites'], function () {
   gulp.src('./assets/css/main.styl')
-    .pipe(stylus({use: [nib()]}))
+    .pipe(stylus({
+      errors: true,
+      use: [nib()]
+    }))
     .pipe(gulp.dest('./public/css'));
 });
 
@@ -15,6 +47,8 @@ gulp.task('nib', function () {
 var scripts = [
   './assets/js/main.js',
 ];
+
+// TODO: add minifier
 
 gulp.task('concat', function() {
   return gulp.src(scripts)
@@ -29,4 +63,4 @@ gulp.task('watch', function() {
 });
 
 // RUN TASKS ------------------------------------------------------------- //
-gulp.task('default', ['nib', 'concat', 'watch']);
+gulp.task('default', ['sprites', 'nib', 'concat', 'watch']);
