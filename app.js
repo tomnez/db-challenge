f70ae42ab89b7ebfcce11e2e30c5c846808c880c
Tom Netzband <tomnez@gmail.com>
2014-09-03 00:05:12 -0400

    spriteSheet: added spritesheet and sprite classes

--- app.js
+++ app.js
@@ -1,9 +1,53 @@
 var express      = require('express')
-  , app          = express();
+  , app          = express()
+  , fs           = require('fs')
+  , Builder      = require('node-spritesheet').Builder;
 
 // Set static directory
 // ------------------------------------------------------------- //
 app.use(express.static(__dirname + '/public')); 
+
+// Configure Spritesheet
+// ------------------------------------------------------------- //
+fs.readdir('./assets/css/spritesrc', function(err, files) {
+  if (err) { return console.error(err); }
+
+  if (files.length) {
+    var fullPathImages = [];
+    for (var i = files.length - 1; i >= 0; i--) {
+      var image = files[i]+'';
+
+      // if not hidden and is an accepted image type
+      if (/^[^\.]+\.(jpg|jpeg|png|gif)$/.test(image)) {
+
+        var fullPathFile    = './assets/css/spritesrc/' + image
+          , fullPathRenamed = './assets/css/spritesrc/' + image.replace('@', '-');
+
+        // rename file to replace '@' with '-' to avoid stylus conflicts
+        fs.renameSync(fullPathFile, fullPathRenamed);
+
+        fullPathImages.push(fullPathRenamed);
+      }
+    };
+
+    var builder = new Builder({
+      outputDirectory: './assets/css/sprites',
+      outputImage: 'sprite.png',
+      outputCss: 'sprites.styl',
+      selector: '.sprite',
+      images: fullPathImages
+    });
+
+    builder.build(function() {
+      console.log("Built from " + builder.files.length + " images");
+
+      fs.rename('./assets/css/sprites/sprite.png', './public/css/sprite.png', function(err) {
+        if (err) { return console.error(err); }
+        console.log("Successfully moved sprite.png into public folder");
+      });
+    });
+  }
+});
 
 // Homepage Route
 // ------------------------------------------------------------- //
