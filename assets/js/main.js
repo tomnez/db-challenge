841dfaef7bea65cc22a60385b8fd6a0d2b32b75a
Tom Netzband <tomnez@gmail.com>
2014-09-07 23:04:02 -0400

    responsiveNavigation: add rules for open/close mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,16 @@
   // DOM ready functionality
   $(document).ready(function() {
 
+    // listener to open/close menu
+    var siteWrap = $('.site-wrap')
+      , menuButton = $('.menu-container');
+
+    if (menuButton.length) {
+      menuButton.on('click', function() {
+        siteWrap.toggleClass('opened');
+      });
+    }
+
   });
 
   // anythang else
